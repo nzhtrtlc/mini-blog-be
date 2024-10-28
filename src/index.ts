55139531d3ba9c9878b2be9b4ec61995b990aaab
nzhtrtlc <20neziht@gmail.com>
2024-10-28 17:05:53 +0300

    deployment config

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,31 @@
-import express from "express";
-import cors from "cors";
-import dotenv from "dotenv";
-import { postRoutes } from "./routes/post.routes";
-import { authRoutes } from "./routes/auth.routes";
-import { commentRoutes } from "./routes/comment.routes";
+import express from 'express'
+import cors from 'cors'
+import dotenv from 'dotenv'
+import { postRoutes } from './routes/post.routes'
+import { authRoutes } from './routes/auth.routes'
+import { commentRoutes } from './routes/comment.routes'
 
-dotenv.config();
+dotenv.config()
 
-const app = express();
+const app = express()
+
+// @ts-ignore
+app.get('/', (req, res) => res.send('Express on Vercel'))
 
 // Middlewares
-app.use(cors());
-app.use(express.json());
+app.use(cors())
+app.use(express.json())
 
 // Routes
-app.use("/api/auth", authRoutes);
-app.use("/api/posts", postRoutes);
-app.use("/api/posts/:postId/comments", commentRoutes);
+app.use('/api/auth', authRoutes)
+app.use('/api/posts', postRoutes)
+app.use('/api/posts/:postId/comments', commentRoutes)
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000
 
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+  console.log(`Server is running on port ${PORT}`)
+})
+
+
+export default app
